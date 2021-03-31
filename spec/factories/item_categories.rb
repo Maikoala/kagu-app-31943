1f39af0138974c873b3741337fde029383d8a2cb
Maikoala <maiko.s.w.0824@gmail.com>
2021-03-31 16:59:59 +0900

    Formオブジェクト'item_category'のテストコード実装

---
+++ spec/factories/item_categories.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :item_category do
+    name             { Faker::House.furniture }
+    info             { Faker::Lorem.characters(number: 50) }
+    category_name_id { Faker::Number.between(from: 2, to: 20) }
+    color_id         { Faker::Number.between(from: 2, to: 13) }
+    price            { Faker::Number.between(from: 1000, to: 100000) }
+  end
+end
--- spec/factories/shops.rb
+++ spec/factories/shops.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :shop do
     name  { Faker::Lorem.words }
     info  { Faker::Lorem.characters(number: 30) }
+    association :owner
   end
 end
