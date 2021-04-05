2b5f7e28009cab549a8624b084b8bbfdc599a9f9
Maikoala <maiko.s.w.0824@gmail.com>
2021-04-05 16:19:50 +0900

    購入機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,15 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :set_item, only: [:index, :create]
 
   def index
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
-
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
@@ -18,8 +18,20 @@ class OrdersController < ApplicationController
   end
 
   private
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
 
   def order_params
-    params.require(:order_address).permit(:first_name, :last_name, :postal_code, :state_province_id, :city, :address_line_1, :address_line_2, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id] )
+    params.require(:order_address).permit(:first_name, :last_name, :postal_code, :state_province_id, :city, :address_line_1, :address_line_2, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,18 +1,19 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :first_name, :last_name, :postal_code, :state_province_id, :city, :address_line_1, :address_line_2, :phone_number
+  attr_accessor :token, :user_id, :item_id, :first_name, :last_name, :postal_code, :state_province_id, :city, :address_line_1, :address_line_2, :phone_number
   
   with_options presence: true do
+    validates :token
     validates :user_id
     validates :item_id
     validates :first_name, :last_name,
               format: { with: /\A[ぁ-んァ-ン一-龥々]/,
-                        message: 'Full-width characters' }
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'Input correctly' }
-    validates :state_province_id, numericality: { other_than: 1, message: 'Select' }
+                        message: 'full-width japanese characters' }
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'input correctly' }
+    validates :state_province_id, numericality: { other_than: 1, message: 'select' }
     validates :city
     validates :address_line_1
-    validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'Input only number' }
+    validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'input only number' }
   end
 
   def save
