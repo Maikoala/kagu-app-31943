499ec3aeeeee609277a26af9801b7e51952d536a
Maikoala <maiko.s.w.0824@gmail.com>
2021-04-10 14:52:47 +0900

    オーナーマイページ実装

---
+++ app/controllers/owners_controller.rb
@@ -0,0 +1,12 @@
+class OwnersController < ApplicationController
+  before_action :authenticate_owner!, only: [:show]
+  before_action :move_to_index, only: [:show]
+
+  def show
+  end
+
+  def move_to_index
+    @owner = Owner.find(params[:id])
+    redirect_to root_path unless current_owner == @owner
+  end
+end
