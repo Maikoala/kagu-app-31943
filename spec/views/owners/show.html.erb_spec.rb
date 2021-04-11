1552e07cfbdb407ab7afffc80d3a70e37d0a13ab
Maikoala <maiko.s.w.0824@gmail.com>
2021-04-11 14:05:56 +0900

    rubocopによる修正

--- spec/views/owners/show.html.erb_spec.rb
+++ spec/views/owners/show.html.erb_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe "owners/show.html.erb", type: :view do
+RSpec.describe 'owners/show.html.erb', type: :view do
   pending "add some examples to (or delete) #{__FILE__}"
 end
