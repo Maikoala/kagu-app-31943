3549fd24b6feb545b520013d71191a48d7d0d40f
Maikoala <maiko.s.w.0824@gmail.com>
2021-04-12 19:52:10 +0900

    トップページ修正

---
+++ app/javascript/search_shop.js
@@ -0,0 +1,15 @@
+function shop () {
+  const Shop = document.getElementById("form-shops");
+  const ShopList = document.getElementById("form-shop-list");
+  Shop.addEventListener('mouseover', () => {
+    Shop.setAttribute("style", "color: #de4d4d;");
+  });
+  Shop.addEventListener('mouseout', () => {
+    Shop.removeAttribute("style", "color: #de4d4d;");
+  });
+  Shop.addEventListener('click', () => {
+    ShopList.setAttribute("style", "display: block;");
+  });
+}
+
+window.addEventListener('load', shop);
