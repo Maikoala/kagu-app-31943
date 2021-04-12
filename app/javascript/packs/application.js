e5d2acf199a74ae81f5e893d35a9cf3fcab018dd
Maikoala <maiko.s.w.0824@gmail.com>
2021-04-12 23:28:01 +0900

    商品検索結果一覧表示フロント実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,9 +8,9 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require("../card")
-require("../search")
+require("../top_search")
 require("../shop")
-require("../search_shop")
+require("../search")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
