3c529593dc674c44b96940ffa0119283a0562de3
Maikoala <maiko.s.w.0824@gmail.com>
2021-04-14 15:55:12 +0900

    商品出品ページフロント実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,7 @@ require("../card")
 require("../top_search")
 require("../shop")
 require("../search")
+require("../price")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
