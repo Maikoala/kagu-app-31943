0d5e9b5d4dfb9e3f60a468ef1020b34246dc8101
Maikoala <maiko.s.w.0824@gmail.com>
2021-05-01 16:51:26 +0900

    トップページフロント実装修正

--- app/javascript/shop.js
+++ app/javascript/shop.js
@@ -1,19 +1,21 @@
 function shop () {
-  const Shop = document.getElementById("shops");
-  const ShopList = document.getElementById("shop-list");
-  Shop.addEventListener('mouseover', () => {
-    Shop.setAttribute("style", "color: #de4d4d;");
-  });
-  Shop.addEventListener('mouseout', () => {
-    Shop.removeAttribute("style", "color: #de4d4d;");
-  });
-  Shop.addEventListener('click', () => {
-    if (ShopList.getAttribute("style") == "display: block;"){
-    ShopList.removeAttribute("style", "display: block;");
-    }else{
-    ShopList.setAttribute("style", "display: block;");
-    };
-  });
+  if (document.getElementById('shops')){
+    const Shop = document.getElementById("shops");
+    const ShopList = document.getElementById("shop-list");
+    Shop.addEventListener('mouseover', () => {
+      Shop.setAttribute("style", "color: #de4d4d;");
+    });
+    Shop.addEventListener('mouseout', () => {
+      Shop.removeAttribute("style", "color: #de4d4d;");
+    });
+    Shop.addEventListener('click', () => {
+      if (ShopList.getAttribute("style") == "display: block;"){
+      ShopList.removeAttribute("style", "display: block;");
+      }else{
+      ShopList.setAttribute("style", "display: block;");
+      };
+    });
+  };
 }
 
 window.addEventListener('load', shop);
--- app/javascript/top_search.js
+++ app/javascript/top_search.js
@@ -1,19 +1,21 @@
 function search () {
-  const topPageSearch = document.getElementById("top-page-search");
-  const searchForm = document.getElementById("item-search-form");
-  topPageSearch.addEventListener('mouseover', () => {
-    topPageSearch.setAttribute("style", "color: #de4d4d;");
-  });
-  topPageSearch.addEventListener('mouseout', () => {
-    topPageSearch.removeAttribute("style", "color: #de4d4d;");
-  });
-  topPageSearch.addEventListener('click', () => {
-    if (searchForm.getAttribute("style") == "display: block;"){
-    searchForm.removeAttribute("style", "display: block;");
-    }else{
-    searchForm.setAttribute("style", "display: block;");
-    };
-  });
+  if (document.getElementById('top-page-search')){
+    const topPageSearch = document.getElementById("top-page-search");
+    const searchForm = document.getElementById("item-search-form");
+    topPageSearch.addEventListener('mouseover', () => {
+      topPageSearch.setAttribute("style", "color: #de4d4d;");
+    });
+    topPageSearch.addEventListener('mouseout', () => {
+      topPageSearch.removeAttribute("style", "color: #de4d4d;");
+    });
+    topPageSearch.addEventListener('click', () => {
+      if (searchForm.getAttribute("style") == "display: block;"){
+      searchForm.removeAttribute("style", "display: block;");
+      }else{
+      searchForm.setAttribute("style", "display: block;");
+      };
+    });
+  }
 }
 
 window.addEventListener('load', search);
