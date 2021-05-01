88e1b15d603c75163ccd88c3f1f8a6c07886e37d
Maikoala <maiko.s.w.0824@gmail.com>
2021-05-01 16:52:55 +0900

    商品出品ページフロント実装修正

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,12 +1,14 @@
 function price () {
-  const inputPrice = document.getElementById('item-price');
-  const taxPrice = document.getElementById('tax-price');
-  const excludingTax = document.getElementById('excluding-tax');
-  inputPrice.addEventListener('input', () => {
-    const tenPercent = Math.floor(inputPrice.value * 0.1);
-    taxPrice.innerHTML = tenPercent;
-    excludingTax.innerHTML = inputPrice.value - tenPercent;
-  });
+  if (document.getElementById('item-price')){
+    const inputPrice = document.getElementById('item-price');
+    const taxPrice = document.getElementById('tax-price');
+    const excludingTax = document.getElementById('excluding-tax');
+    inputPrice.addEventListener('input', () => {
+      const tenPercent = Math.floor(inputPrice.value * 0.1);
+      taxPrice.innerHTML = tenPercent;
+      excludingTax.innerHTML = inputPrice.value - tenPercent;
+    });
+  };
 }
 
 window.addEventListener('load', price);
