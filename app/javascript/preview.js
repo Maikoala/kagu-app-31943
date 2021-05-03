8b8e765c6f9b31b541360d540c3e12d953039321
Maikoala <maiko.s.w.0824@gmail.com>
2021-05-03 18:48:22 +0900

    商品投稿複数画像選択JavaScript実装

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -4,17 +4,33 @@ document.addEventListener('DOMContentLoaded', function(){
 
     const createImageHTML = (blob) => {
       const imageElement = document.createElement('div');
+      imageElement.setAttribute('class', "image-element")
+      let imageElementNum = document.querySelectorAll('.image-element').length
+
       const blobimage = document.createElement('img');
       blobimage.setAttribute('src', blob);
       blobimage.setAttribute('class', 'form-image-preview');
 
+      const inputHTML = document.createElement('input');
+      inputHTML.setAttribute('id', `item_image_${imageElementNum}`);
+      inputHTML.setAttribute('name', 'item[images][]');
+      inputHTML.setAttribute('type', 'file')
+
       imageElement.appendChild(blobimage);
+      imageElement.appendChild(inputHTML)
       ImagePreview.appendChild(imageElement);
+
+      inputHTML.addEventListener('change', (e) => {
+        file = e.target.files[0];
+        blob = window.URL.createObjectURL(file);
+
+        createImageHTML(blob)
+      });
     };
 
     document.getElementById('item_image').addEventListener('change', function(e){
-      const file = e.target.files[0];
-      const blob = window.URL.createObjectURL(file);
+      let file = e.target.files[0];
+      let blob = window.URL.createObjectURL(file);
 
     createImageHTML(blob);
     });
